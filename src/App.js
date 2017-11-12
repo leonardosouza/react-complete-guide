dd80166a48333c2b38f91da858ee813aa560ed3c
Leonardo Souza <leonardodisouza@gmail.com>
2017-11-11 23:18:28 -0200

    Understanding & Using State

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import Person from './Person/Person';
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Barbara', age: 9, pic: 'http://www.lorempixel.com.br/100/100/1' },
+      { name: 'Davi', age: 3, pic: 'http://www.lorempixel.com.br/100/100/2' },
+      { name: 'Caio', age: 17, pic: 'http://www.lorempixel.com.br/100/100/3' }
+    ]
+  };
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Person name="Barbara" age="9"><img src="http://www.lorempixel.com.br/100/100" /></Person>
-        <Person name="Davi" age="3">My hobbies: toys</Person>
-        <Person name="Caio" age="17">My hobbies: video-games</Person>
-      </div>
-    );
+    return <div className="App">
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>
+          <img src={this.state.persons[0].pic} alt={this.state.persons[0].name} />
+        </Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>
+          My hobbies: toys
+        </Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>
+          My hobbies: video-games
+        </Person>
+      </div>;
   }
 }
 
