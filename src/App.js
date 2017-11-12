73f7bb0246166be03807da449896bbee24debdc8
Leonardo Souza <leonardodisouza@gmail.com>
2017-11-11 22:45:15 -0200

    Creating a Functional Component and Output Dynamic Content

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Person from './Person/Person';
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -13,6 +14,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Person />
+        <Person />
+        <Person />
       </div>
     );
   }
