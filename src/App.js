2bb8c7a70578e63d3f5fc80ed6ee18d66151950c
Leonardo Souza <leonardodisouza@gmail.com>
2017-11-18 23:08:51 -0200

    Outputting Lists

--- src/App.js
+++ src/App.js
@@ -5,33 +5,11 @@ import './App.css';
 class App extends Component {
   state = {
     persons: [
-      { name: 'Barbara', age: 9, pic: 'http://www.lorempixel.com.br/100/100/1' },
-      { name: 'Davi', age: 3, pic: 'http://www.lorempixel.com.br/100/100/2' },
-      { name: 'Caio', age: 17, pic: 'http://www.lorempixel.com.br/100/100/3' }
+      { id: 1, name: 'Barbara', age: 9, pic: 'http://www.lorempixel.com.br/100/100/?1' },
+      { id: 2, name: 'Davi', age: 3, pic: 'http://www.lorempixel.com.br/100/100/?2' },
+      { id: 3, name: 'Caio', age: 17, pic: 'http://www.lorempixel.com.br/100/100/?3' }
     ],
     showPersons: false
-  }
-
-  switchNameHandler = (...args) => {
-    let name = typeof args[0] === 'string' ? args[0] : 'Person';
-    this.setState({
-      persons: [
-        { name, age: 10, pic: 'http://www.lorempixel.com.br/100/100/1' },
-        { name, age: 4, pic: 'http://www.lorempixel.com.br/100/100/2' },
-        { name, age: 18, pic: 'http://www.lorempixel.com.br/100/100/3' }
-      ]
-    })
-  }
-
-  nameChangeHandler = (event, ...args) => {
-    let name = typeof args[0] === 'string' ? args[0] : 'Person';
-    this.setState({
-      persons: [
-        { name, age: 10, pic: 'http://www.lorempixel.com.br/100/100/1' },
-        { name, age: 4, pic: 'http://www.lorempixel.com.br/100/100/2' },
-        { name: event.target.value, age: 18, pic: 'http://www.lorempixel.com.br/100/100/3' }
-      ]
-    })
   }
 
   togglePersonsHandler = () => {
@@ -54,15 +32,11 @@ class App extends Component {
     if(this.state.showPersons) {
       persons = (
         <div>
-          <Person name={this.state.persons[0].name} age={this.state.persons[0].age} click={this.switchNameHandler.bind(this, 'Babiiiii')} change={this.nameChangeHandler}>
-            <img src={this.state.persons[0].pic} alt={this.state.persons[0].name} />
-          </Person>
-          <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={() => this.switchNameHandler('Daviiiiii')} change={this.nameChangeHandler}>
-            My hobbies: toys
-          </Person>
-          <Person name={this.state.persons[2].name} age={this.state.persons[2].age} click={this.switchNameHandler} change={this.nameChangeHandler}>
-            My hobbies: video-games
-          </Person>
+          {this.state.persons.map(person => {
+            return <Person name={person.name} age={person.age} key={person.id} change={() => {}}>
+                <img src={person.pic} alt={person.name} />
+              </Person>;
+          })}
         </div>
       )
     }
