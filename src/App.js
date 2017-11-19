209b7f5852279f2f91ae018d4e37772ba81480bf
Leonardo Souza <leonardodisouza@gmail.com>
2017-11-18 22:18:47 -0200

    Rendering Content Conditionally

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import Person from './Person/Person';
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -9,7 +8,8 @@ class App extends Component {
       { name: 'Barbara', age: 9, pic: 'http://www.lorempixel.com.br/100/100/1' },
       { name: 'Davi', age: 3, pic: 'http://www.lorempixel.com.br/100/100/2' },
       { name: 'Caio', age: 17, pic: 'http://www.lorempixel.com.br/100/100/3' }
-    ]
+    ],
+    showPersons: false
   }
 
   switchNameHandler = (...args) => {
@@ -34,29 +34,39 @@ class App extends Component {
     })
   }
 
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons
+    this.setState({ showPersons : !doesShow })
+  }
+
   render() {
     let style = {
-      border: '2px solid',
-      background: 'yellow',
-      cursor: 'pointer',
-      marginTop: '20px'
+      button : {
+        border: '2px solid',
+        background: 'yellow',
+        cursor: 'pointer',
+        marginTop: '20px'
+      }
     }
 
-    return (
-      <div className="App">
-        <button onClick={this.switchNameHandler} style={style}>Switch name</button>
+    return <div className="App">
+        {/* <button onClick={this.switchNameHandler} style={style}>Switch name</button> */}
+        <button onClick={this.togglePersonsHandler} style={style.button}>
+          Show / Hide Names
+        </button>
 
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} click={this.switchNameHandler.bind(this, 'Babiiiii')}>
-          <img src={this.state.persons[0].pic} alt={this.state.persons[0].name} />
-        </Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={() => this.switchNameHandler('Daviiiiii')}>
-          My hobbies: toys
-        </Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} click={this.switchNameHandler} change={this.nameChangeHandler}>
-          My hobbies: video-games
-        </Person>
-      </div>
-    );
+        {this.state.showPersons ? <div>
+            <Person name={this.state.persons[0].name} age={this.state.persons[0].age} click={this.switchNameHandler.bind(this, 'Babiiiii')} change={this.nameChangeHandler}>
+              <img src={this.state.persons[0].pic} alt={this.state.persons[0].name} />
+            </Person>
+            <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={() => this.switchNameHandler('Daviiiiii')} change={this.nameChangeHandler}>
+              My hobbies: toys
+            </Person>
+            <Person name={this.state.persons[2].name} age={this.state.persons[2].age} click={this.switchNameHandler} change={this.nameChangeHandler}>
+              My hobbies: video-games
+            </Person>
+          </div> : null}
+      </div>;
   }
 }
 
