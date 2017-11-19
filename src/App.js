b4236a7fc2cdbf9e876bca5062bc8b567b3b0393
Leonardo Souza <leonardodisouza@gmail.com>
2017-11-18 23:23:31 -0200

    Lists & State

--- src/App.js
+++ src/App.js
@@ -5,45 +5,82 @@ import './App.css';
 class App extends Component {
   state = {
     persons: [
-      { id: 1, name: 'Barbara', age: 9, pic: 'http://www.lorempixel.com.br/100/100/?1' },
-      { id: 2, name: 'Davi', age: 3, pic: 'http://www.lorempixel.com.br/100/100/?2' },
-      { id: 3, name: 'Caio', age: 17, pic: 'http://www.lorempixel.com.br/100/100/?3' }
+      {
+        id: 1,
+        name: 'Barbara',
+        age: 9,
+        pic: 'http://www.lorempixel.com.br/100/100/?1',
+      },
+      {
+        id: 2,
+        name: 'Davi',
+        age: 3,
+        pic: 'http://www.lorempixel.com.br/100/100/?2',
+      },
+      {
+        id: 3,
+        name: 'Caio',
+        age: 17,
+        pic: 'http://www.lorempixel.com.br/100/100/?3',
+      },
+      {
+        id: 4,
+        name: 'Luan',
+        age: 20,
+        pic: 'http://www.lorempixel.com.br/100/100/?4',
+      },
     ],
-    showPersons: false
-  }
+    showPersons: false,
+  };
 
   togglePersonsHandler = () => {
-    const doesShow = this.state.showPersons
-    this.setState({ showPersons : !doesShow })
-  }
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+  };
+
+  deletePersonHandler = personIndex => {
+    const persons = this.state.persons;
+    persons.splice(personIndex, 1);
+    this.setState({ persons });
+  };
 
   render() {
     let style = {
-      button : {
+      button: {
         border: '2px solid',
         background: 'yellow',
         cursor: 'pointer',
-        marginTop: '20px'
-      }
-    }
+        marginTop: '20px',
+      },
+    };
 
     let persons = null;
 
-    if(this.state.showPersons) {
+    if (this.state.showPersons) {
       persons = (
         <div>
-          {this.state.persons.map(person => {
-            return <Person name={person.name} age={person.age} key={person.id} change={() => {}}>
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
+                name={person.name}
+                age={person.age}
+                key={person.id}
+                change={() => {}}
+                click={() => this.deletePersonHandler(index)}
+              >
                 <img src={person.pic} alt={person.name} />
-              </Person>;
+              </Person>
+            );
           })}
         </div>
-      )
+      );
     }
 
     return (
       <div className="App">
-        <button onClick={this.togglePersonsHandler} style={style.button}>Toggle Names</button>
+        <button onClick={this.togglePersonsHandler} style={style.button}>
+          Toggle Names
+        </button>
         {persons}
       </div>
     );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -5,11 +5,10 @@ const person = (props) => {
   const { name, age, children, click, change } = props;
   return (
     <div className="Person">
-      <p onClick={click}>
-        I'm a <strong>{name}</strong> and I <strong>{age}</strong> years old!
-      </p>
+      <p>I'm <strong>{name}</strong> and I <strong>{age}</strong> years old!</p>
       <p>{children}</p>
       <p><input onChange={change} value={name} /></p>
+      <button onClick={click}>Remove this item</button>
     </div>
   );
 }
