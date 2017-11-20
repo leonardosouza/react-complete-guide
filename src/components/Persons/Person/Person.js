e033085df6a2ec1b09e10612933a8ad908784fce
Leonardo Souza <leonardodisouza@gmail.com>
2017-11-20 12:19:33 -0200

    Converting Stateless to Stateful Components

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Person.css';
 
-const person = (props) => {
-  const { name, age, children, clicked, changed, cssClasses } = props; 
+class Person extends Component {
+  render () {
+    const { name, age, children, clicked, changed, cssClasses } = this.props;
 
-  return <div className={`${cssClasses} ${classes.upper} ${classes.Person}`}>
-      <p>
-        I'm <strong>{name}</strong> and I <strong>{age}</strong> years old!
-      </p>
-      <p>{children}</p>
-      <p>
-        <input onChange={changed} value={name} />
-      </p>
-      <button onClick={clicked}>Remove this item</button>
-    </div>;
+    return <div className={`${cssClasses} ${classes.upper} ${classes.Person}`}>
+        <p>
+          I'm <strong>{name}</strong> and I <strong>{age}</strong> years old!
+        </p>
+        <p>{children}</p>
+        <p>
+          <input onChange={changed} value={name} />
+        </p>
+        <button onClick={clicked}>Remove this item</button>
+      </div>;
+  }
 }
 
-export default person;
+export default Person;
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,24 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 import classes from './Persons.css';
 
-const persons = props => {
-  let cssClasses = [];
-  cssClasses.push(props.persons.length > 2 ? classes.greenyellow : classes.darkorange);
+class Persons extends Component {
+  render () {
+    let cssClasses = [];
+    cssClasses.push(this.props.persons.length > 2 ? classes.greenyellow : classes.darkorange);
   
-  return props.persons.map((person, index) => {
-    return <Person
-      key={person.id}
-      name={person.name}
-      age={person.age}
-      changed={(event) => props.changed(event, person.id)}
-      clicked={() => props.clicked(index)}
-      cssClasses={cssClasses.join(' ')}
-    >
-      <img src={person.pic} alt={person.name} />
-    </Person>
-  })
+    return this.props.persons.map((person, index) => {
+      return <Person
+        key={person.id}
+        name={person.name}
+        age={person.age}
+        changed={(event) => this.props.changed(event, person.id)}
+        clicked={() => this.props.clicked(index)}
+        cssClasses={cssClasses.join(' ')}
+      >
+        <img src={person.pic} alt={person.name} />
+      </Person>
+    })
+  }
+}
 
-};
-
-export default persons;
+export default Persons;
